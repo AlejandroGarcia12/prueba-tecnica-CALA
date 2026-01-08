16bc578305d01973d049dc7a1275d6125eaa0942
Alejandro Garcia <alejandrogarciasanchez35@gmail.com>
2026-01-08 17:52:47 -0500

    remove model

--- analytics_portal/core/models.py
+++ analytics_portal/core/models.py
@@ -1,7 +1,3 @@
 from django.db import models
 
-class DailySales(models.Model):
-    date = models.DateField(null=True, blank=True)
-    client_id = models.CharField(max_length=100, blank=True)
-    product = models.CharField(max_length=100, blank=True)
-    value_sale = models.IntegerField(null=True, blank=True)
+# Create your models here.
