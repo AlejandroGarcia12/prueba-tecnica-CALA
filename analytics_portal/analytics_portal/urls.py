431b57f6fb956af45425dd13662e826d43be1aea
Alejandro Garcia <alejandrogarciasanchez35@gmail.com>
2026-01-08 17:51:12 -0500

    Solucion bloque 1

---
+++ analytics_portal/analytics_portal/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import include, path
+
+from core import views as core_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', core_views.home, name='home'),
+    path('dashboard/', include('dashboard.urls')),
+]
